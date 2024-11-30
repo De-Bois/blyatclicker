b7c45c04cb3a88c24e077ab8398435d0c5f69b1d
Mark Boute <bmark0702@gmail.com>
2024-12-01 00:26:01 +0100

    replace default vue project with simple app and add global color vars

---
+++ blyue/src/components/SovietClicker.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+import { ref } from 'vue';
+
+const score = ref(0);
+
+function incrementScore() {
+    score.value++;
+};
+
+setInterval(() => {
+    score.value++;
+}, 1000);
+
+</script>
+<template>
+
+    <div class="clicker">
+        <h1>Soviet Clicker</h1>
+        <p>Click the button to increase your score!</p>
+        <button @click="incrementScore">Click me!</button>
+        <p>Score: {{ score }}</p>
+    </div>
+
+</template>
+<style scoped>
+
+.clicker {
+    color: var(--color-accent);
+    background-color: var(--color-background);
+    border-radius: 15px;
+    padding: 20px;
+    border: 5px solid var(--color-component-border);
+}
+
+</style>
